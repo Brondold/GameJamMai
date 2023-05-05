0888846611f0533b1e452fc1d26fa2816502d39d
Hugo Carrasco <carrasco.hugo.hc@gmail.com>
2023-05-05 11:13:35 +0200

    van cle fini

--- Souvenirs/Assets/Hugo/2Scripts/S_Park.cs
+++ Souvenirs/Assets/Hugo/2Scripts/S_Park.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class S_Park : MonoBehaviour
 {
-    public GameObject Manager;
     public GameObject SelfHandle;
     public Transform SelfParkPlace;
 
@@ -13,20 +12,11 @@ public class S_Park : MonoBehaviour
     
     public void OnTriggerEnter (Collider other) 
     {
-        print("Collision");
         if (other.gameObject.name == TargetPark) 
         {
             InArea = true;
             SelfHandle.GetComponent<S_Drag>().DisableAvai();
             transform.parent.position = SelfParkPlace.position;
-            StartCoroutine(Delay());
         }
-   }
-
-   public IEnumerator Delay()
-   {
-        yield return new WaitForSeconds(.1f);
-        print("tourne");
-        Manager.GetComponent<S_ParkingManager>().Add();
    }
 }
